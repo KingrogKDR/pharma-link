13011f9767fba5c51048ed3dafac4f2e54a2802d
KingrogKDR <abhisheksaikia766@gmail.com>
2024-07-09 14:13:20 +0530

    connect db

---
+++ server/db/database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE pharma_link;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import pool from '../db/db.js'
 
 const app = express()
+const dbPool = pool
 
+// middlewares
 app.use(express.urlencoded({extended: true, limit: '16kb'}))
 app.use(express.static('public'))
-
+app.use(express.json())
 
 // routes
 
